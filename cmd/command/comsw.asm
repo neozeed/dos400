43df989a26f78b06ac86f3173e101a9a5efc7670
Jason Stevens <jason.r.stevens@live.com>
2024-04-26 01:19:11 +0100

    MS-DOS 4.00 from Microsoft

---
+++ cmd/command/comsw.asm
@@ -0,0 +1,5 @@
+;	SCCSID = @(#)comsw.asm	1.1 85/05/14
+;	SCCSID = @(#)comsw.asm	1.1 85/05/14
+
+include version.inc
+
---
+++ dev/printer/4208/4208-cpy.asm
@@ -0,0 +1,9 @@
+
+CODE	segment public 'code'
+ASSUME	CS:CODE,DS:NOTHING,ES:NOTHING,SS:NOTHING
+	org	0
+
+	include copyrigh.inc
+
+code	ends
+	end
